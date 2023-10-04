de5d1abd72a1e13c199940e8156cb3c15ccfa906
Suparan <suparansharmap@gmail.com>
2023-10-05 00:11:58 +0600

    update

---
+++ anisul islam/redux-toolkit/src/features/posts/postSlice.js
@@ -0,0 +1,20 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts",async()=>{
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    return res.data;
+})
+
+const postSlice = createSlice({
+    name:'posts',
+    initialState:{
+        isLoading:false,
+        posts:[],
+        error:null
+    },
+    extraReducers:(builder)=>{
+
+    }
+})
