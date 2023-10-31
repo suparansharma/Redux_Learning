04f8f9cd866b83643d97b87b083791bd5e197bd0
Suparan <suparansharmap@gmail.com>
2023-11-01 00:44:36 +0600

    update

--- next_lvl/tech-net/src/App.jsx
+++ next_lvl/tech-net/src/App.jsx
@@ -1,9 +1,39 @@
+
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './redux/features/counter/counterSlice';
 function App() {
+  const {count} = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
-    <>
-      <h1 className="bg-red-500">Tech next</h1>
-    </>
+    <div>
+    <div>
+      <button
+        aria-label="Increment value"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+
+      <button
+        aria-label="Increment value"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+        onClick={() => dispatch(incrementByAmount(5))}
+      >
+        IncrementBy5
+      </button>
+      <span className="text-xl font-semibold">{count}</span>
+      <button
+        aria-label="Decrement value"
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+    </div>
+  </div>
+  
   )
 }
 
---
+++ next_lvl/tech-net/src/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./redux/features/counter/counterSlice";
+
+
+
+const store = configureStore({
+    reducer:{
+        counter : counterReducer,
+    },
+})
+
+export default store;
