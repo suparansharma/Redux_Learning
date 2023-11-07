3a2402004a1737fe4a1e6eace0dbca1fece1bba2
Suparan Sharma <suparansharma@gmail.com>
2023-11-07 21:26:48 +0600

    update

--- next_lvl/tech-net/src/store.js
+++ next_lvl/tech-net/src/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./redux/features/counter/counterSlice";
 
-
-
 const store = configureStore({
     reducer:{
         counter : counterReducer,
